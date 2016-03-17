b34a7081b50b1d71791f9f6d8f3b1587db71a6ed
CJ Griscom <cjgriscom@comcast.net>
2016-03-17 13:45:36 -0500

    Add printStackTrace option to log

--- src/com/quirkygaming/errorlib/ErrorHandler.java
+++ src/com/quirkygaming/errorlib/ErrorHandler.java
@@ -13,6 +13,7 @@ public class ErrorHandler<T extends Exception> {
 	private HashSet<Class<?>> handledExceptions = new HashSet<Class<?>>();
 	private HashSet<Class<?>> unwrappedExceptions = new HashSet<Class<?>>();
 	private PrintStream log;
+	private boolean printStackTrace = false;
 	
 	public static ErrorHandler<Exception> forwardHandled(Logger log, Class<?>... handledExceptions) {
 		return forwardHandled(new PrintStream(new LoggerStream(log, Level.SEVERE)), handledExceptions);
@@ -20,11 +21,17 @@ public class ErrorHandler<T extends Exception> {
 	public static ErrorHandler<RuntimeException> logAll(Logger log) {
 		return logAll(new PrintStream(new LoggerStream(log, Level.SEVERE)));
 	}
+	public static ErrorHandler<RuntimeException> logAll(Logger log, boolean printStackTrace) {
+		return logAll(new PrintStream(new LoggerStream(log, Level.SEVERE)), printStackTrace);
+	}
 	public static ErrorHandler<Exception> forwardHandled(PrintStream log, Class<?>... handledExceptions) {
 		return new ErrorHandler<Exception>(log, false, Exception.class, handledExceptions);
 	}
 	public static ErrorHandler<RuntimeException> logAll(PrintStream log) {
-		return new ErrorHandler<RuntimeException>(log, false, RuntimeException.class);
+		return logAll(log, false);
+	}
+	public static ErrorHandler<RuntimeException> logAll(PrintStream log, boolean printStackTrace) {
+		return new ErrorHandler<RuntimeException>(log, false, printStackTrace, RuntimeException.class);
 	}
 	public static ErrorHandler<Exception> forwardHandled(Class<?>... handledExceptions) {
 		return new ErrorHandler<Exception>(null, false, Exception.class, handledExceptions);
@@ -37,6 +44,12 @@ public class ErrorHandler<T extends Exception> {
 		this.log = log;
 		Collections.addAll(this.handledExceptions, handledExceptions);
 	}
+	
+	private ErrorHandler(PrintStream log, boolean bypassMode, boolean printStackTrace, Class<T> mode, Class<?>... handledExceptions) {
+		this(log, bypassMode, mode, handledExceptions);
+		this.printStackTrace = printStackTrace;
+	}
+
 	
 	public ErrorHandler<T> addHandledExceptions(Class<?>... exceptionTypes) {
 		Collections.addAll(handledExceptions, exceptionTypes);
@@ -65,6 +78,7 @@ public class ErrorHandler<T extends Exception> {
 				}
 			} else {
 				StackTraceElement ste = e.getStackTrace()[0];
+				if (printStackTrace) e.printStackTrace(log);
 				log.println("Caught " + e.getClass().getName() + ": " + e.getMessage() + " at line " + ste.getLineNumber() + " in " + ste.getClassName());
 			}
 		}
